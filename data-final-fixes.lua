90f90956d7af6da959e3f2f96e65c5cb618dba10
blushies <11660219+blushies@users.noreply.github.com>
2026-09-13 21:49:14 -0700

    fix: The lava glow in the trench can no longer be destroyed

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -172,3 +172,12 @@ end
 if data.raw["explosion"]["nuke-effects-nauvis"] then
     PlanetsLib.restrict_surface_conditions(data.raw["explosion"]["nuke-effects-nauvis"], {property = "pressure", max = 50000})
 end
+
+--- The trench's lava glow is made possible by these lamps. This prevents them
+--- from being able to be destroyed, including by damage types added by other
+--- mods.
+local lava_lamp = data.raw["simple-entity"]["maraxsis-lava-lamp"]
+lava_lamp.resistances = {}
+for damage_type in pairs(data.raw["damage-type"]) do
+    table.insert(lava_lamp.resistances, {type = damage_type, percent = 100})
+end
